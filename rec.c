b66bef6999bc0ce2bb99a88c3a86bcc76a089c45
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2023-05-07 17:18:25 +0530

    rec sturcture

--- rec.c
+++ rec.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
-int area(int l, int b);
-int getlenth();
-int setlength(int l);
 struct rec
 {
     int length;
     int breadth;
-}r = {10,5};
+}r;
+int area(struct rec r);
+int getlenth();
+int setlength(int l);
 void main()
 {
+    printf("Enter the size of length and breadth\t");
+    scanf("%d%d",r.length,r.breadth);
+    printf("AREA = %d\n",area(r));
     printf("length = %d\n",getlenth());
-    printf("AREA = %d\n",area(r.length,r.breadth));
     setlength(2);
     printf("length = %d\n",getlenth());
 }
-int area(int l, int b)
+int area(struct rec r)
     {
-        return l*b;
+        return r.length*r.breadth;
     }
 int getlenth()
 {
